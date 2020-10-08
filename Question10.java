cfffc9b3df4ef163611dca8306dac24574e63bea
5Anisa <71705614+5Anisa@users.noreply.github.com>
2020-10-08 19:57:25 +0200

    Add files via upload

---
+++ Question10.java
@@ -0,0 +1,17 @@
+package day_3;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		
+		
+		int hour=12;
+		int minute=24;
+		int second=33;
+		String am="AM", pm="PM";
+		
+		System.out.println(hour + ":"+ minute + ":" + second + pm);
+		
+	}
+
+}
