94c8cd3948d2659ef5138d75fc6c00d1b43f96a0
5Anisa <71705614+5Anisa@users.noreply.github.com>
2020-10-08 19:55:51 +0200

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,17 @@
+package day_3;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		
+		
+		String n1="James", n2="Betty", n3="Herb", n4="Bob", n5="Jill";
+		
+		System.out.println(n1 + ", "+n2 + ", "+n3 + ", "+n4 + ", "+n5);
+		
+ 		
+		
+
+	}
+
+}
