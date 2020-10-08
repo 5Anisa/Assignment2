f975557df6a75751c85f588ec57d2b5234434edc
5Anisa <71705614+5Anisa@users.noreply.github.com>
2020-10-08 19:57:00 +0200

    Add files via upload

---
+++ Question9.java
@@ -0,0 +1,21 @@
+package day_3;
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		
+		
+		byte steps=100;
+		short miles=5000;
+		int count=1000000;
+		long population=334455667;
+		
+		
+		System.out.println(steps);
+		System.out.println(miles);
+		System.out.println(count);
+		System.out.println(population);
+		
+	}
+
+}
