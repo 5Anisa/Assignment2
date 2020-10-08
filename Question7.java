eae4007a8237518df97ee87bf9ec5afe2fcc1dd2
5Anisa <71705614+5Anisa@users.noreply.github.com>
2020-10-08 19:56:16 +0200

    Add files via upload

---
+++ Question7.java
@@ -0,0 +1,21 @@
+package day_3;
+
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+
+		int i=20;
+		float f=4.5f;
+		
+		
+		System.out.println("i = " + i + "f = " + f);
+		
+	
+	
+	}
+	
+	
+
+}
