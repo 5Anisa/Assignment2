2489c0c90e4781dcab84b97dbe56b6b463da4984
5Anisa <71705614+5Anisa@users.noreply.github.com>
2020-10-08 19:55:22 +0200

    Add files via upload

---
+++ Question5.java
@@ -0,0 +1,14 @@
+package day_3;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		
+		
+		int first=25, second=45;
+		
+		System.out.println("first is " + first + "second " + second);
+
+	}
+
+}
